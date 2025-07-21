379152e8dd0e38c901cae6d587838051cbad0c88
jerry <jerry@mg-consulting-it-act.com>
2025-07-21 16:19:42 +0300

    feat: init front app

---
+++ front/src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-3xl font-semibold text-primary">
+        Hello world !
+
+        <Button>
+            Cliquer
+        </Button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from 'primevue';
+</script>
